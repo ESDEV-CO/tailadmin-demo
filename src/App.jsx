fe2c51730c1f261d9e796234b4b64d15e4836a91
shahab-edraak <shahab@edraaksystems.com>
2023-07-26 10:53:33 +0500

    fragments removed

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,19 @@ import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <>
-      <div className="dash_container">
-        <aside className="container_side">
-          <Aside />
-        </aside>
-        <div className="container_dash">
-          <div className="dash_nav">
-            <Navbar />
-          </div>
-          <div className="dash_db">
-            <Dashboard />
-          </div>
+    <div className="dash_container">
+      <aside className="container_side">
+        <Aside />
+      </aside>
+      <div className="container_dash">
+        <div className="dash_nav">
+          <Navbar />
+        </div>
+        <div className="dash_db">
+          <Dashboard />
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
